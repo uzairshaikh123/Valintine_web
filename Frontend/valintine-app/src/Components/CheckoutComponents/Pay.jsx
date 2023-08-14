5357961c7a36eddb92aa1ceb3da2bd08c0e5eded
uzair <uzair@metatechitsolutions.com>
2023-08-14 15:39:36 +0530

    sum fixes

--- Frontend/valintine-app/src/Components/CheckoutComponents/Pay.jsx
+++ Frontend/valintine-app/src/Components/CheckoutComponents/Pay.jsx
@@ -2,43 +2,41 @@ import React from 'react'
 import axios from 'axios';
 import { Button } from '@chakra-ui/react';
 const Pay = () => {
-	// 7e3e98f0-9b56-4d76-a669-df0be0399253
+
 	const body = {
-		"merchantId": "VALENTINEONLINE",
-		"merchantTransactionId": "MT7850590068188104",
-		"merchantUserId": "MU933037302229373",
-		"amount": 1,
-		"callbackUrl": "https://webhook.site/callback-url",
-		"mobileNumber": "9999999999",
-		"deviceContext": {
-		  "deviceOS": "ANDROID"
+		method: 'POST',
+		url: 'https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay',
+		headers: {
+			accept: 'application/json',
+			'Content-Type': 'application/json',
+			'X-VERIFY': '2cd3a2cd04ff5b453df11e91f97b29fae10fe22522a29c1ae6b7fad41478d8b5###1'
 		},
-		"paymentInstrument": {
-		  "type": "UPI_INTENT",
-		  "targetApp": "com.phonepe.app",
-		  "accountConstraints": [{					//Optional. Required only for TPV Flow.
-			  "accountNumber": "420200001892",
-			  "ifsc": "ICIC0000041"
-		  }]
+		data: {
+			request: 'ewogICJtZXJjaGFudElkIjogIk1FUkNIQU5UVUFUIiwKICAibWVyY2hhbnRUcmFuc2FjdGlvbklkIjogIk1UNzg1MDU5MDA2ODE4ODEwNCIsCiAgIm1lcmNoYW50VXNlcklkIjogIk1VSUQxMjMiLAogICJhbW91bnQiOiAxMDAwMCwKICAicmVkaXJlY3RVcmwiOiAiaHR0cHM6Ly93ZWJob29rLnNpdGUvcmVkaXJlY3QtdXJsIiwKICAicmVkaXJlY3RNb2RlIjogIlBPU1QiLAogICJjYWxsYmFja1VybCI6ICJodHRwczovL3dlYmhvb2suc2l0ZS9jYWxsYmFjay11cmwiLAogICJtb2JpbGVOdW1iZXIiOiAiOTk5OTk5OTk5OSIsCiAgInBheW1lbnRJbnN0cnVtZW50IjogewogICAgInR5cGUiOiAiUEFZX1BBR0UiCiAgfQp9'
 		}
-	  }
-	  const handle_payments = ()=>{
-
-		  axios.post('https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay',body)
-			.then(response => response.json())
-			.then(response => console.log(response))
-			.catch(err => console.error(err));
-	  }
+	};
+	const handle_payments = () => {
 
 
+		axios
+			.request(body)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	}
 
 
 
-  return (
-    <div style={{width:"100%"}}>
-		<Button onClick={handle_payments}>Pay</Button>
-    </div>
-  )
+
+
+	return (
+		<div style={{ width: "100%" }}>
+			<Button onClick={handle_payments}>Pay</Button>
+		</div>
+	)
 }
 
 export default Pay
