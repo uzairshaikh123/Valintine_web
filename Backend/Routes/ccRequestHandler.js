7fe1747d53f0f8dfd36851148d01ee1622687508
uzair <uzair@metatechitsolutions.com>
2023-08-24 16:32:11 +0530

    payment integration check

--- Backend/Routes/ccRequestHandler.js
+++ Backend/Routes/ccRequestHandler.js
@@ -1,73 +1,74 @@
-// var http = require('http'),
-//     fs = require('fs'),
-//     ccav2 = require('./ccutils.js'),
-//     qs = require('querystring');
-// const crypto = require('crypto');
-// const express = require('express');
-// require("dotenv").config()
-// const ccRouter = express.Router()
+var http = require('http'),
+    fs = require('fs'),
+    ccav = require('./ccutils.js'),
+    qs = require('querystring');
+const crypto = require('crypto');
+const express = require('express');
+require("dotenv").config()
+const ccRouter = express.Router()
 // const nodeCCAvenue = require('node-ccavenue');
 
 
 
-// ccRouter.post("/", (request,response)=>{
+ccRouter.post("/", (request,response)=>{
 
-//     console.log("body",request.body)
-    
-//     // var encRequest = crypto.createHash('sha256').update(JSON.stringify(request.body)).digest('hex');
-//     var body = JSON.stringify(request.body),
-//     workingKey = process.env.Working_Key,	//Put in the 32-Bit key shared by CCAvenues.
-//     accessCode = process.env.Access_key_cc,			//Put in the Access Code shared by CCAvenues.
-//     formbody = '';
-    
-//     // console.log(encRequest)
-//     // request.on('data', function () {
-//         const encRequest = ccav.getEncryptedOrder(body);
-//         // data+=body
-//         // encRequest = ccav.node_encrypt_ccavenue_request(body,workingKey); 
-//     formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
-//     // });
-                
-//     response.send(formbody);
-//     // request.on('end', function () {
-//     // });
-//     return; 
-// })
-
-// module.exports = ccRouter
-var http = require('http'),
-    fs = require('fs'),
-    // ccav = require('./ccavutil.js'),
-    qs = require('querystring');
-const nodeCCAvenue = require('node-ccavenue');
-
-exports.postReq = function(request,response){
-
-console.log(request.body)
-
-
-const ccav = new nodeCCAvenue.Configure({
-  merchant_id: process.env.Merchant_ID,
-  working_key: process.env.Working_Key,
-});
-
-    var body = request.body,	//Put in the 32-Bit key shared by CCAvenues.
-    accessCode = process.env.Access_key_cc,			//Put in the Access Code shared by CCAvenues.
+    console.log("body",request.body)
+    var body = request.body,
+    merchant_id= process.env.Merchant_ID,
+    working_key=process.env.Working_Key,			//Put in the Access Code shared by CCAvenues.
 	encRequest = '',
 	formbody = '';
 				
     request.on('data', function (data) {
 	body += data;
-    encRequest = ccav.getEncryptedOrder(body);
-	
+	encRequest = ccav.encrypt(body,working_key); 
 	formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
     });
 				
     request.on('end', function () {
-        response.writeHeader(200, {"Content-Type": "text/html"});
+    response.writeHeader(200, {"Content-Type": "text/html"});
 	response.write(formbody);
 	response.end();
     });
    return; 
-};
+    
+})
+
+module.exports = ccRouter
+
+
+
+
+// var http = require('http'),
+//     fs = require('fs'),
+//     // ccav = require('./ccavutil.js'),
+//     qs = require('querystring');
+// const nodeCCAvenue = require('node-ccavenue');
+// app.post('/ccavRequestHandler', function (request, response){
+//     console.log(request.body)
+//     const ccav = new nodeCCAvenue.Configure({
+//         merchant_id: process.env.Merchant_ID,
+//         working_key: process.env.Working_Key,
+//     });
+    
+//     var body = request.body,	//Put in the 32-Bit key shared by CCAvenues.
+//     accessCode = process.env.Access_key_cc,			//Put in the Access Code shared by CCAvenues.
+// 	encRequest = '',
+// 	formbody = '';
+    
+//     request.on('data', function (data) {
+//         body += data;
+//         encRequest = ccav.getEncryptedOrder(body);
+        
+//         formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+//     });
+    
+//     request.on('end', function () {
+//         response.writeHeader(200, {"Content-Type": "text/html"});
+//         response.write(formbody);
+//         response.end();
+//     });
+//     return; 
+// });
+// };
 
