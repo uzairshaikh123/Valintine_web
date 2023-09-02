3e94662f64fd07ab71e5d1f65ba08beb9628e590
uzair <uzair@metatechitsolutions.com>
2023-09-02 17:49:07 +0530

    sum fix

--- Backend/Routes/ccRequestHandler.js
+++ Backend/Routes/ccRequestHandler.js
@@ -94,9 +94,11 @@ exports.postReq = function(request,response){
     //         customer_identifier, // to save card info on payment gateway side
     //     } = req.body;
     //Generate Md5 hash for the key and then convert in base64 string
-    var md5 = crypto.createHash('md5').update("59A3CAD0524DC498CB70302C1357320A").digest();
+    
+    var md5 = crypto.createHash('md5').update(process.env.Working_Key).digest();
     var keyBase64 = Buffer.from(md5).toString('base64');
     
+
     //Initializing Vector and then convert in base64 string
     var ivBase64 = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,0x0e, 0x0f]).toString('base64');
     // let enc=(ccav.encrypt(JSON.stringify(body), keyBase64, ivBase64))
@@ -125,11 +127,9 @@ exports.postReq = function(request,response){
         
     response.render("payment", {
                     encryptedData: encryptionResponseData,
-                    access_code: "AVNN05KI16AD13NNDA", // your access_code provided by bank
+                    access_code: process.env.Access_key_cc, // your access_code provided by bank
                     // access_code: process.env.Access_key_cc, // your access_code provided by bank
                 });
    return; 
 };
 
-
-// AVNN05KI16AD13NNDA
