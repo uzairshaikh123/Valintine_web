9cd9d86250bb35ed1c7118ba12b1e8cbe119c9ff
Uzair <uzairmd073@gmail.com>
2023-09-09 16:34:21 +0530

    res adjust

--- Backend/Routes/ccResponseHanlder.js
+++ Backend/Routes/ccResponseHanlder.js
@@ -41,7 +41,7 @@ exports.postRes = function(request,response){
 			htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
 			response.writeHeader(200, {"Content-Type": "text/html"});
 			response.write(htmlcode);
-           response.send({msg:"Status",status:order_status});
+           response.end();
 		   return
         }
 
@@ -54,7 +54,7 @@ exports.postRes = function(request,response){
         htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
         response.writeHeader(200, {"Content-Type": "text/html"});
 	    response.write(htmlcode);
-		return response.send({msg:"Status",status:order_status});
+		return response.end();
 		
 	
 };
