acfb076a60f990af880b7f86e94e8357220192b5
Uzair <uzairmd073@gmail.com>
2024-04-08 10:49:57 +0530

    something added in the project

--- Frontend/valintine-app/src/Components/AdminComponents/AddBlogs.jsx
+++ Frontend/valintine-app/src/Components/AdminComponents/AddBlogs.jsx
@@ -1,18 +1,29 @@
-
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { handle_add_blog } from '../../Redux/action';
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { handle_add_blog } from "../../Redux/action";
 
 const AddBlogs = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const dispatch = useDispatch()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
   const handlesubmit = (e) => {
     e.preventDefault();
 
     const authorheading = document.querySelector(".author-heading").value;
 
-    const authordescription = document.querySelector(".author-description").value;
+    const authordescription = document.querySelector(
+      ".author-description"
+    ).value;
 
     const authorname = document.querySelector(".author-name").value;
 
@@ -35,15 +46,15 @@ const AddBlogs = () => {
     const subimage3 = document.querySelector(".sub-image-3").value;
 
     const subcontent3 = document.querySelector(".sub-content-3").value;
-    const title = document.querySelector(".title").value
-    const category = document.querySelector(".category").value
-    const city = document.querySelector(".city").value
-    const heading = document.querySelector(".heading").value
-    const subheading = document.querySelector(".subheading").value
-    const blogdate = document.querySelector(".blog-date").value
-    const userId = document.querySelector(".userId").value
-    const metatags = document.querySelector(".metatags").value
-    const hashtags = document.querySelector(".hashtags").value
+    const title = document.querySelector(".title").value;
+    const category = document.querySelector(".category").value;
+    const city = document.querySelector(".city").value;
+    const heading = document.querySelector(".heading").value;
+    const subheading = document.querySelector(".subheading").value;
+    const blogdate = document.querySelector(".blog-date").value;
+    const userId = document.querySelector(".userId").value;
+    const metatags = document.querySelector(".metatags").value;
+    const hashtags = document.querySelector(".hashtags").value;
     let obj = {
       category,
       city,
@@ -54,29 +65,45 @@ const AddBlogs = () => {
       userID: userId,
       metatags,
       hashtags,
-      comments:[],
-      title, authorheading, authordescription, authorname, authordate, authorimage,
-      blogimage, blogdescription, subimage1, subcontent1, subimage2, subcontent2,
-      subimage3, subcontent3
-    }
-    dispatch(handle_add_blog(obj)).then((res)=>{
-      if(res.status==200 || res.status==201){
-        alert("Blog Added successfully")
-      }else{
-        alert("Blog Not Added Error")
-
+      comments: [],
+      title,
+      authorheading,
+      authordescription,
+      authorname,
+      authordate,
+      authorimage,
+      blogimage,
+      blogdescription,
+      subimage1,
+      subcontent1,
+      subimage2,
+      subcontent2,
+      subimage3,
+      subcontent3,
+    };
+    dispatch(handle_add_blog(obj)).then((res) => {
+      if (res.status == 200 || res.status == 201) {
+        alert("Blog Added successfully");
+      } else {
+        alert("Blog Not Added !Error: " + res.status);
       }
-    })
-    onClose()
-
-  }
-
-
+    });
+    onClose();
+  };
 
   return (
     <div>
-
-      <Button style={{float:"right",color:"white",background:"blue",display:"block"}} onClick={onOpen}>ADD BLOG</Button>
+      <Button
+        style={{
+          float: "right",
+          color: "white",
+          background: "blue",
+          display: "block",
+        }}
+        onClick={onOpen}
+      >
+        ADD BLOG
+      </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -85,88 +112,161 @@ const AddBlogs = () => {
           <ModalCloseButton />
           <ModalBody>
             <form onSubmit={handlesubmit}>
-            
-
               <label>TITLE</label>
-              <input type="text" placeholder="Enter Title" className="title"></input>
+              <input
+                type="text"
+                placeholder="Enter Title"
+                className="title"
+              ></input>
 
               <label>CATEGORY</label>
-              <input type="text" placeholder="Enter Category" className="category"></input>
+              <input
+                type="text"
+                placeholder="Enter Category"
+                className="category"
+              ></input>
 
               <label>CITY</label>
-              <input type="text" placeholder="Enter City" className="city"></input>
-
+              <input
+                type="text"
+                placeholder="Enter City"
+                className="city"
+              ></input>
 
               <label>HEADING</label>
-              <input type="text" placeholder="Enter heading" className="heading"></input>
+              <input
+                type="text"
+                placeholder="Enter heading"
+                className="heading"
+              ></input>
 
               <label>SUBHEADING</label>
-              <input type="text" placeholder="Enter Subheading " className="subheading"></input>
-
+              <input
+                type="text"
+                placeholder="Enter Subheading "
+                className="subheading"
+              ></input>
 
               <label>BLOG DATE </label>
-              <input type="text" placeholder="Enter Blog Date" className="blog-date"></input>
+              <input
+                type="text"
+                placeholder="Enter Blog Date"
+                className="blog-date"
+              ></input>
 
               <label>USERID</label>
-              <input type="text" placeholder="Enter UserID" className="userId"></input>
+              <input
+                type="text"
+                placeholder="Enter UserID"
+                className="userId"
+              ></input>
 
               <label>METATAGS</label>
-              <input type="text" placeholder="Enter Metatags" className="metatags"></input>
+              <input
+                type="text"
+                placeholder="Enter Metatags"
+                className="metatags"
+              ></input>
 
               <label>HASHTAGS</label>
-              <input type="text" placeholder="Enter AuthorNam" className="hashtags"></input>
+              <input
+                type="text"
+                placeholder="Enter AuthorNam"
+                className="hashtags"
+              ></input>
 
               <label>AUTHOR HEADING</label>
-              <input type="text" placeholder="Enter AuthorHeading" className="author-heading"></input>
+              <input
+                type="text"
+                placeholder="Enter AuthorHeading"
+                className="author-heading"
+              ></input>
 
               <label>AUTHOR DESCRIPTION</label>
-              <input type="text" placeholder="Enter author description" className="author-description"></input>
+              <input
+                type="text"
+                placeholder="Enter author description"
+                className="author-description"
+              ></input>
 
               <label>AUTHOR NAME</label>
-              <input type="text" placeholder="Enter author name" className="author-name"></input>
+              <input
+                type="text"
+                placeholder="Enter author name"
+                className="author-name"
+              ></input>
 
               <label>AUTHOR DATE</label>
-              <input type="text" placeholder="Enter author date" className="author-date"></input>
+              <input
+                type="text"
+                placeholder="Enter author date"
+                className="author-date"
+              ></input>
 
               <label>AUTHOR IMAGE</label>
-              <input type="text"  placeholder="Enter authorimage url" className="author-image"></input>
+              <input
+                type="text"
+                placeholder="Enter authorimage url"
+                className="author-image"
+              ></input>
 
               <label>BLOG IMAGE</label>
-              <input type="text" placeholder="Enter Blog Url"  className="blog-image"></input>
+              <input
+                type="text"
+                placeholder="Enter Blog Url"
+                className="blog-image"
+              ></input>
 
               <label>BLOG DESCRIPTION</label>
-              <input type="" placeholder="Enter blog description" className="blog-description"></input>
+              <input
+                type=""
+                placeholder="Enter blog description"
+                className="blog-description"
+              ></input>
 
               <label>SUB IMAGE 1</label>
-              <input type="" placeholder="Enter sub image 1" className="sub-image-1"></input>
+              <input
+                type=""
+                placeholder="Enter sub image 1"
+                className="sub-image-1"
+              ></input>
 
               <label>SUB CONTENT 1</label>
-              <input type="" placeholder="Enter sub content 1" className="sub-content-1"></input>
+              <input
+                type=""
+                placeholder="Enter sub content 1"
+                className="sub-content-1"
+              ></input>
 
               <label>SUB IMAGE 2</label>
               <input type="text" className="sub-image-2"></input>
 
               <label>SUB CONTENT 2</label>
-              <input type="" placeholder="Enter sub content 2" className="sub-content-2"></input>
+              <input
+                type=""
+                placeholder="Enter sub content 2"
+                className="sub-content-2"
+              ></input>
 
               <label>SUB IMAGE 3</label>
               <input type="text" className="sub-image-3"></input>
 
               <label>SUB CONTENT 3</label>
-              <input type="text" placeholder="Enter sub content 3" className="sub-content-3"></input>
+              <input
+                type="text"
+                placeholder="Enter sub content 3"
+                className="sub-content-3"
+              ></input>
 
-              <Button mt={"10px"} type="submit">Add Blog</Button>
-
+              <Button mt={"10px"} type="submit">
+                Add Blog
+              </Button>
             </form>
           </ModalBody>
         </ModalContent>
       </Modal>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default AddBlogs
-
-
+export default AddBlogs;
